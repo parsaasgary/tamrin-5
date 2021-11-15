539d5591db3bbae576b133a44be309b936fbb0d6
parsaasgary <92506180+parsaasgary@users.noreply.github.com>
2021-11-15 05:53:45 -1200

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,16 @@
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i, j;
+    for (i = 1; i <= 10; i++)
+    {
+        for (j = 1; j <= 10; j++)
+            cout << i * j << "\t";
+        cout << "\n";
+    }
+}
+
---
+++ 3.cpp
@@ -0,0 +1,31 @@
+﻿#include <conio.h>
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+//سید پارسا عسگری طباطبائی
+
+int main()
+{
+	char s[50];
+	int a[50];
+	int i=0, t=0, j = 0,m=0;
+	cin.get(s,50);
+	while (s[i] != '.')
+	{
+		m++;
+		if (s[i] == ' ')
+		{
+			t++;
+			a[j] =m-1 ;
+			j++;
+			m = 0;
+		}
+		i++;
+			
+	}
+	cout << "tedad kalamat:" << t<<"\n";
+	for (j = 0; j < t; j++)
+		cout << "tedad horof kalame"<<j+1<<":" << a[j] << "\t";
+}
+
